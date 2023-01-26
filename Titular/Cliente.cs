4f848e2546097194d97dc009d31f274c2220dff6
yuri-miotto <120820142+yuri-miotto@users.noreply.github.com>
2023-01-26 19:37:00 -0300

    Add files via upload

---
+++ Titular/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank.Titular
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Profissao { get; set; }
+    }
+}
+
